cfac6c86dd75b3d0f75a53ab89c3aa1f2b117d2e
John Smith <xfgryujk@126.com>
2018-04-09 21:29:20 +0800

    Add about and build

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ memegen
 Generates meme image.
 
 [Online demo](https://xfgryujk.github.io/memegen/)
+
+![demo](https://github.com/xfgryujk/memegen/blob/master/static/sorry/example.gif)
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -7,7 +7,13 @@
         <b-nav-item href="#" v-b-modal.about-modal>关于</b-nav-item>
           <b-modal id="about-modal" centered ok-only title="关于">
             <p>
-              WIP
+              这是一个在线制作沙雕图（表情图）的web app。受到<a href="https://sorry.xuty.tk/sorry/">在线生成Sorry动图</a>的启发，不过这是完全在前端实现的，在我不知道的时候前端已经变得这么强大了呢。目前只能制作GIF图，以后会加入静态图和更多模板。
+            </p>
+            <p>
+              GIF解码用的是<a href="https://github.com/deanm/omggif">omggif</a>，编码用的是<a href="https://github.com/jnordberg/gif.js">gif.js</a>的优化版。只在Chrome和Edge下测试过，如果不能用请使用最新的Chrome浏览器。
+            </p>
+            <p>
+              作者：xfgryujk。<a href="https://github.com/xfgryujk/memegen">获取源码</a>
             </p>
           </b-modal>
       </b-navbar-nav>
